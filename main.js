baabec4dd220c1a31d49f49b653faa06b8142cdb
AdolfoPB <adolfo.pvw@gmail.com>
2023-04-18 22:10:47 -0400

    branch nuevo

--- main.js
+++ main.js
@@ -4,38 +4,38 @@ function calcular(){
     tipo = document.getElementById('tipo').value;
 
     console.log(tipo);
-    // if(tipo == "suma"){
-    //     var result = valor1 + valor2;
-    // }else if(tipo == "resta"){
-    //     var result = valor1 - valor2;
-    // }else if(tipo == "multiplicacion"){
-    //     var result = valor1 * valor2;
-    // }else if(tipo == "division"){
-    //     var result = valor1 / valor2;
-    // }else if(tipo == "modulo"){
-    //     var result = valor1 % valor2;
-    // }
+    if(tipo == "suma"){
+        var result = valor1 + valor2;
+    }else if(tipo == "resta"){
+        var result = valor1 - valor2;
+    }else if(tipo == "multiplicacion"){
+        var result = valor1 * valor2;
+    }else if(tipo == "division"){
+        var result = valor1 / valor2;
+    }else if(tipo == "modulo"){
+        var result = valor1 % valor2;
+    }
 
-    switch (tipo) {
-        case "suma":
-            var result = valor1 + valor2;
-          break;
-        case "resta":
-            var result = valor1 - valor2;
-          break;
-        case "multiplicacion":
-            var result = valor1 * valor2;
-          break;
-          case "division":
-            var result = valor1 / valor2;
-          break;
-          case "modulo":
-            var result = valor1 % valor2;
-          break;
-        default:
-          alert('Eliga una opcion');
-          break;
-      }
+    // switch (tipo) {
+    //     case "suma":
+    //         var result = valor1 + valor2;
+    //       break;
+    //     case "resta":
+    //         var result = valor1 - valor2;
+    //       break;
+    //     case "multiplicacion":
+    //         var result = valor1 * valor2;
+    //       break;
+    //       case "division":
+    //         var result = valor1 / valor2;
+    //       break;
+    //       case "modulo":
+    //         var result = valor1 % valor2;
+    //       break;
+    //     default:
+    //       alert('Eliga una opcion');
+    //       break;
+    //   }
     
 
     document.getElementById('result').value = result;
